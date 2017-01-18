f6218731b5c1832c1180b6a79b7e2ea73cf26c64
wesflynn <wesley.flynn@mygeorgian.ca>
2017-01-18 14:34:41 -0500

    new output for game of war

--- src/war/WAR.java
+++ src/war/WAR.java
@@ -18,53 +18,62 @@ public class WAR {
      */
    public static void main(String[] args) {
         
-        for (int count = 100; count>0; count -= 5)
-            System.out.println(count);
-    
-        System.out.printf("If we enter 31, there is enough snow for skiing: %b%n",timeToSki(31));
-        System.out.printf("If we enter 29, there is enough snow for skiing: %b%n",timeToSki(29));
+       GameOfWar theGame = new GameOfWar();
+       theGame.playTheGame();
+       
+       
+       
+       
+       
+       
+       
+//        for (int count = 100; count>0; count -= 5)
+//            System.out.println(count);
+//    
+//        System.out.printf("If we enter 31, there is enough snow for skiing: %b%n",timeToSki(31));
+//        System.out.printf("If we enter 29, there is enough snow for skiing: %b%n",timeToSki(29));
+//        
+//        starDisplay(50);
         
-        starDisplay(50);
-        
-        Card myFirstCard = new Card("Ace", "Spades", 14);
-        System.out.printf("My card is: %s%n", myFirstCard);
-        
-        Card mySecondCard = new Card("Ace", "hearts", 14);
-        System.out.printf("My card is: %s%n", mySecondCard);
-        
-        DeckOfCards theDeck = new DeckOfCards();
-        System.out.printf("Our deck of cards includes: %n%s", theDeck.toString());
-        
-        theDeck.shuffle();
-        
-        System.out.printf("%n%nAfter shuffling the deck, it looks like: %n%s", theDeck.toString());
-        
-        
-        ArrayList<Card> zacsHand = new ArrayList<>();
-        for (int i =1 ; i <= 5; i++)
-        {
-            zacsHand.add(theDeck.dealTopCard());
-        }
-        
-        System.out.printf("%nZac's hand is: %s", zacsHand.toString());
+//        Card myFirstCard = new Card("Ace", "Spades", 14);
+//        System.out.printf("My card is: %s%n", myFirstCard);
+//        
+//        Card mySecondCard = new Card("Ace", "hearts", 14);
+//        System.out.printf("My card is: %s%n", mySecondCard);
+//        
+//        DeckOfCards theDeck = new DeckOfCards();
+//        System.out.printf("Our deck of cards includes: %n%s", theDeck);
+//        
+//        theDeck.shuffle();
+//        
+//        System.out.printf("%n%nAfter shuffling the deck, it looks like: %n%s", theDeck);
+//        
+//        
+//        ArrayList<Card> zacsHand = new ArrayList<>();
+//        for (int i =1 ; i <= 5; i++)
+//        {
+//            zacsHand.add(theDeck.dealTopCard());
+//        }
+//        
+//        System.out.printf("\nZac's hand is: %s\n", zacsHand.toString());
     }  // end of the main method
     
     
-    public static boolean timeToSki(int snowDepth)
-    {
-        return snowDepth >  30;
-    }
-    
-    public static void starDisplay(int lines)
-    {
-        for ( ; lines > 0 ; lines--)
-        {
-            for (int i = 1; i <= lines; i++)
-                System.out.print("*");
-            
-            System.out.println();
-        }
-    }
+//    public static boolean timeToSki(int snowDepth)
+//    {
+//        return snowDepth >  30;
+//    }
+//    
+//    public static void starDisplay(int lines)
+//    {
+//        for ( ; lines > 0 ; lines--)
+//        {
+//            for (int i = 1; i <= lines; i++)
+//                System.out.print("*");
+//            
+//            System.out.println();
+//        }
+//    }
     
     
 }
