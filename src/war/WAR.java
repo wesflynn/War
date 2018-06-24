636ab39fa017b56e9d26cba30a56cd3dabc1a894
Wesley Flynn <wesley.flynn@mygeorgian.ca>
2018-06-23 21:22:45 -0400

    Update WAR.java

--- src/war/WAR.java
+++ src/war/WAR.java
@@ -1,79 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package war;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author wes_4
- */
 public class WAR {
 
-    /**
-     * @param args the command line arguments
-     */
    public static void main(String[] args) {
         
        GameOfWar theGame = new GameOfWar();
        theGame.playTheGame();
-       
-       
-       
-       
-       
-       
-       
-//        for (int count = 100; count>0; count -= 5)
-//            System.out.println(count);
-//    
-//        System.out.printf("If we enter 31, there is enough snow for skiing: %b%n",timeToSki(31));
-//        System.out.printf("If we enter 29, there is enough snow for skiing: %b%n",timeToSki(29));
-//        
-//        starDisplay(50);
-        
-//        Card myFirstCard = new Card("Ace", "Spades", 14);
-//        System.out.printf("My card is: %s%n", myFirstCard);
-//        
-//        Card mySecondCard = new Card("Ace", "hearts", 14);
-//        System.out.printf("My card is: %s%n", mySecondCard);
-//        
-//        DeckOfCards theDeck = new DeckOfCards();
-//        System.out.printf("Our deck of cards includes: %n%s", theDeck);
-//        
-//        theDeck.shuffle();
-//        
-//        System.out.printf("%n%nAfter shuffling the deck, it looks like: %n%s", theDeck);
-//        
-//        
-//        ArrayList<Card> zacsHand = new ArrayList<>();
-//        for (int i =1 ; i <= 5; i++)
-//        {
-//            zacsHand.add(theDeck.dealTopCard());
-//        }
-//        
-//        System.out.printf("\nZac's hand is: %s\n", zacsHand.toString());
     }  // end of the main method
-    
-    
-//    public static boolean timeToSki(int snowDepth)
-//    {
-//        return snowDepth >  30;
-//    }
-//    
-//    public static void starDisplay(int lines)
-//    {
-//        for ( ; lines > 0 ; lines--)
-//        {
-//            for (int i = 1; i <= lines; i++)
-//                System.out.print("*");
-//            
-//            System.out.println();
-//        }
-//    }
-    
-    
 }
